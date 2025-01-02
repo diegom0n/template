8e4aa3320a6249d0fec52935d2c6ce0deece9a45
diegom0n <die.fuentes@duocuc.cl>
2025-01-02 01:26:26 -0300

    deploy test 6

--- template/src/app/supabase-test/page.tsx
+++
@@ -1,9 +0,0 @@
-import SupabaseTest from "@/components/tests/supabase/SupabaseTest";
-
-export default function Home() {
-  return (
-    <main className="p-6">
-      <SupabaseTest />
-    </main>
-  );
-}
